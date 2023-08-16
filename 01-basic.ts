65e21a6f076241761d34da76452212dea8b17b99
yusohee <ysh7271@chunjae.co.kr>
2023-08-16 17:22:53 +0900

    230816

--- 01-basic.ts
+++ 01-basic.ts
@@ -8,9 +8,13 @@
 // true, false
 
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+	//	typeof 연산자 : 변수 타입을 문자열로 반환해줌
+	//	console.log(typeof true); -> 출력 결과: "boolean"
 	//	if (typeof n1 !== 'number' || typeof n2 !== 'number') {
 	//		throw new Error('틀렸어!');
 	//	}
+
+
 	let result = n1 + n2; // 계산 따로 변수로 담아줌
 	if ( showResult ) {
 		// console.log(phrase + n1 + n2); 문자열과 함께 계산되면 모두 문자열로 반환
--- app.ts
+++
@@ -1,4 +0,0 @@
-// unknown type
-// 어떤 사용자가 무엇을 입력할지 알 수 없을 때
-
-let userInput: unknown;
